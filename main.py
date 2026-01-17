ce76eebe434b8660f5627cf8a03dedb00b797508
lucas <lucas.oklp@gmail.com>
2026-01-16 22:34:35 -0300

    untill here RAG fished based in HastagProgramacao course

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_groq import ChatGroq 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 CAMINHO_DB = "db"
 
-prrompt_template = """ Responda a pergunta do usuario:
+prompt_template = """ Responda a pergunta do usuario:
 {pergunta}
 
 Com base nessas informacoes:
 
-{Ba se_conhecimento}
+{Base_conhecimento}
 
 Se você não encontrar a resposta para a pergunta do usuario nessas informacoes, responda não sei te dizer isso"""
 
-pergunta = input("Escreva sua pergunta: ")
+def perguntar():
+    pergunta = input("Escreva sua pergunta: ")
+    funcao_embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    db = Chroma(persist_directory=CAMINHO_DB, embedding_function=funcao_embedding)
 
-funcao_embedding = HuggingFaceEmbeddings()
-db = Chroma(persist_directory=CAMINHO_DB, embedding_function=funcao_embedding)
- 
+    resultados = db.similarity_search_with_relevance_scores (pergunta, k=4)
+    if len(resultados) == 0 or resultados[0][1] < 0.6:
+        print("Nenhum consegiu encontar alguma informação relevante")
+        return
+    textos_resultados = []
+    for resultado in resultados:
+       texto = resultado[0].page_content
+       textos_resultados.append(texto)
+
+    base_conhecimento = "\n\n------\n\n".join(textos_resultados)
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+    prompt = prompt.invoke({"pergunta": pergunta, "Base_conhecimento": base_conhecimento})
+    print(prompt)
+  
+    model = ChatGroq(model="llama-3.3-70b-versatile")
+    texto_resposta = model.invoke(prompt).content
+    print(texto_resposta)
+perguntar()
